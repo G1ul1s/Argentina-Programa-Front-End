dd9881a11b79c2150ca2c8a27f47cf94f3e69182
G1ul1s <88418248+G1ul1s@users.noreply.github.com>
2023-03-26 23:35:30 -0300

    Update educacion.service.ts

--- src/app/servicios/educacion.service.ts
+++ src/app/servicios/educacion.service.ts
@@ -7,7 +7,7 @@ import { Educacion } from '../model/educacion';
   providedIn: 'root'
 })
 export class EducacionService {
-  educURL="http://localhost:8080/educacion/"
+  educURL="https://argentina-programa-back-end-springboot.onrender.com/educacion/"
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Educacion[]>{ 
@@ -30,3 +30,4 @@ public delete(id: number): Observable<any>{
   return this.httpClient.delete<any>(this.educURL + `delete/${id}`);
 }
 }
+
