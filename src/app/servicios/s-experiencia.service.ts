f589efed572cdaa14aa2a53a4c269ab29d9d46a7
G1ul1s <88418248+G1ul1s@users.noreply.github.com>
2023-03-26 23:37:06 -0300

    Update s-experiencia.service.ts

--- src/app/servicios/s-experiencia.service.ts
+++ src/app/servicios/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-expURL="http://localhost:8080/explab/"
+expURL="https://argentina-programa-back-end-springboot.onrender.com/explab/"
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Experiencia[]>{ 
