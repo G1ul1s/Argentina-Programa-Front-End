6abdedbf81755ec66a8a5b0b3f1d4c7a59a60552
G1ul1s <giulianacervi@gmail.com>
2023-04-21 09:42:37 -0300

    actualización front end

---
+++ src/app/componentes/encabezado/encabezado.component.ts
@@ -0,0 +1,83 @@
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from 'src/app/servicios/auth.service';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+@Component({
+  selector: 'app-encabezado',
+  templateUrl: './encabezado.component.html',
+  styleUrls: ['./encabezado.component.css']
+})
+export class EncabezadoComponent implements OnInit {
+  miPortfolio: any; 
+  form:FormGroup;
+  perEdit:any;
+
+  constructor(private datosPortfolio:PortfolioService, private formbuilder: FormBuilder, private autenticado: AuthService) {
+    
+     this.form = this.formbuilder.group({
+
+      id:["",Validators.required],
+      name:["",Validators.required],
+      backImagen:["",Validators.required],
+      imagen:["",Validators.required],
+      position:["",Validators.required],
+      company:["",Validators.required],
+      college:["",Validators.required],
+      location:["",Validators.required],
+      title1:["",Validators.required],
+      about:["",Validators.required],
+      title2:["",Validators.required],
+      email:["",Validators.required],
+      password:["",Validators.required],
+      listEducacion:["",Validators.required],
+      listExperiencia:["",Validators.required],
+      listCertificaciones:["",Validators.required]
+     })
+  }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPortfolio=data;
+    });
+  }
+
+verPersona(perEdit:any): void {
+  
+  this.datosPortfolio.verPersona(perEdit).subscribe(data => {
+
+    this.form.patchValue({
+
+      id:data.id,
+      name:data.name,
+      backImagen:data.backImagen,
+      imagen:data.imagen,
+      position:data.position,
+      company:data.company,
+      college:data.college,
+      location:data.location,
+      title1:data.title1,
+      about:data.about,
+      title2:data.title2,
+      email:data.email,
+      password:data.password,
+      listEducacion:data.listEducacion,
+      listExperiencia:data.listExperiencia,
+      listCertificaciones:data.listCertificaciones
+    })
+    this. perEdit=data;
+    console.log(data);
+  });
+}
+editarPersona():void{
+  this.datosPortfolio.editarPersona(this.form.value).subscribe(data =>{
+    this.ngOnInit();
+  })
+}
+
+logueado(){
+  return this.autenticado.logged();
+ } 
+}
